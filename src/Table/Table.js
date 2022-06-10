c6087483b0c01ad5d0ef13efe3c4639a0628e688
Pyzhov Yakov <pyzovakov@mail.ru>
2022-06-10 11:01:16 +0300

    jsonplaceholder

--- src/Table/Table.js
+++ src/Table/Table.js
@@ -16,23 +16,48 @@ function createData(name, calories, fat) {
     return { name, calories, fat };
 }
 
-const rows = [
-    createData('Cupcake', 305, 3.7),
-    createData('Donut', 452, 25.0),
-    createData('Eclair', 262, 16.0),
-    createData('Frozen yoghurt', 159, 6.0),
-    createData('Gingerbread', 356, 16.0),
-    createData('Honeycomb', 408, 3.2),
-    createData('Ice cream sandwich', 237, 9.0),
-    createData('Jelly Bean', 375, 0.0),
-    createData('KitKat', 518, 26.0),
-    createData('Lollipop', 392, 0.2),
-    createData('Marshmallow', 318, 0),
-    createData('Nougat', 360, 19.0),
-    createData('Oreo', 437, 18.0),
-]
+// const rows = [
+//     createData('Cupcake', 305, 3.7),
+//     createData('Donut', 452, 25.0),
+//     createData('Eclair', 262, 16.0),
+//     createData('Frozen yoghurt', 159, 6.0),
+//     createData('Gingerbread', 356, 16.0),
+//     createData('Honeycomb', 408, 3.2),
+//     createData('Ice cream sandwich', 237, 9.0),
+//     createData('Jelly Bean', 375, 0.0),
+//     createData('KitKat', 518, 26.0),
+//     createData('Lollipop', 392, 0.2),
+//     createData('Marshmallow', 318, 0),
+//     createData('Nougat', 360, 19.0),
+//     createData('Oreo', 437, 18.0),
+// ]
+const rows=[]
 
 export default function CustomPaginationActionsTable() {
+    const [users, setUsers] = React.useState([]);
+    const urlJson= 'https://jsonplaceholder.typicode.com/users';
+
+    React.useEffect(()=>{
+        const fetchData = async ()=>{
+            fetch(urlJson)
+                .then(response => response.json())
+                .then(json => setUsers(json))
+            // const users_data = await axios(urlJson);
+            // setUsers(users_data);
+
+        };
+        fetchData();
+    },[setUsers])
+
+    if(rows.length===0){
+        users.map(user=>rows.push(createData(user.username,user.address.geo.lat,user.address.geo.lng)))
+        console.log(rows)
+    }
+
+
+
+
+
     const [page, setPage] = React.useState(0);
     const [rowsPerPage, setRowsPerPage] = React.useState(5);
     // Avoid a layout jump when reaching the last page with empty rows.
